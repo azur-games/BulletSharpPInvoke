4dcd7e70f3597243f01c074a3e543e060dcff722
Andrey Danilov <danilov.a@azurgames.com>
2022-01-18 19:32:49 +0300

    Remove dependency on `UnityEngine.CoreModule.dll` for server code

---
+++ BulletSharpPInvoke/MonoPInvokeCallback.cs
@@ -0,0 +1,11 @@
+﻿#if !UNITY_2017_1_OR_NEWER
+using System;
+
+namespace AOT
+{
+    public sealed class MonoPInvokeCallback : Attribute
+    {
+        public MonoPInvokeCallback(Type type) { }
+    }
+}
+#endif
