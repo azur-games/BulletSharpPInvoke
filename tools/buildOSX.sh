5979688ed6989423877678c9d89118cd884c6449
Igor Polishchuk <pol-ii@ya.ru>
2022-02-07 13:34:39 +0300

    build osx

--- tools/buildOSX.sh
+++ tools/buildOSX.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cd /libbulletc/libbulletc-OSX/
+cd ./libbulletc/libbulletc-OSX/
 xcodebuild -configuration Release -project libbulletc.xcodeproj
-cp /libbulletc/libbulletc-OSX/build/Release/libbulletc.*         /src/build/OSX/
